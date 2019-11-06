741f83153e0ff89d261cced812d193905c76646c
Hoàng Xuân Cường <cuonghx@cuonghxs-MacBook-Pro.local>
2019-11-06 23:17:23 +0700

    Init Server & Config Mongod

---
+++ server/database.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient
+
+const URL = 'mongodb://localhost:27017/chatapp';
+
+exports.Database = class Database {
+	connect(){
+		return new Promise((resolve, reject) => {
+			MongoClient.connect(URL, (err, db) => {
+				return err ? reject(err) : resolve(db);
+			});
+		});
+	}
+}
